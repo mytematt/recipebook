8d135027f40324b08cf690e3cca096ef8d093604
Matt Wilkinson <mytematt@gmail.com>
2018-11-02 18:53:40 -0600

    recipe model commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_214813) do
+ActiveRecord::Schema.define(version: 2018_11_03_005304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "ingredients"
+    t.text "procedure"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
